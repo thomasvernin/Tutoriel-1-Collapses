5e47fb2fc02d1e9189cb3d4a186b970d2d9eb84e
Thomas Vernin <tvernin49100@gmail.com>
2024-06-14 16:05:56 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleCollapse() {
+    const content = document.querySelector('.collapse-content');
+    const icon = document.querySelector('.collapse-icon');
+    
+    if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+        icon.style.transform = 'rotate(0deg)';
+    } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+        icon.style.transform = 'rotate(180deg)';
+    }
+}
